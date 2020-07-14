b37a2f97a226f1228c9a7908a69af786f161ddc8
hansa <hansajayathilaka@yahoo.com>
2020-07-14 13:58:53 +0000

    finish the front end.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const hbs = require('express-handlebars');
+const Handlebars = require('handlebars')
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+
 
 // Database
 const db = require('./config/database')
@@ -15,7 +18,24 @@ db.authenticate()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('INDEX'));
+// Handlebars
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Index route
+app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
+
 // Gig Routes
 app.use('/gigs', require('./routes/gigs'));
 
--- routes/gigs.js
+++ routes/gigs.js
@@ -2,16 +2,96 @@ const express = require('express')
 const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/gigs')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op;
 
 
+// Get gig list
 router.get('/', (req, res, next) => {
     Gig.findAll()
-        .then(docs => {
-            console.log(docs);
-            res.send(docs);
+        .then(gigs => {
+            res.render('gigs', {
+                gigs
+            })
         })
         .catch(err => console.log(err));
     // res.send('Hello World');
+});
+
+
+// Display Add gig form
+router.get('/add', (req, res, next) => {
+    res.render('add');
+});
+
+
+// Add a gig
+router.post('/add', (req, res, next) => {
+    let {title, technologies, budget, contact_email, description} = req.body;
+    let errors = []
+
+    // Validation
+    if(!title){
+        errors.push({ text: 'Please add a title.' })
+    }
+    if(!technologies){
+        errors.push({ text: 'Please add some technologies.' })
+    }
+    if(!description){
+        errors.push({ text: 'Please add a description.' })
+    }
+    if(!contact_email){
+        errors.push({ text: 'Please add a contact email.' })
+    }
+
+    // Check for errors
+    if(errors.length === 0){
+        if(!budget){
+            budget = 'Unknown';
+        } else {
+            budget = `$${ budget }`;
+        }
+
+        // Make Lower case
+        technologies = technologies.toLowerCase().replace(/, /g, ',')
+
+        // Insert into table
+        Gig.create({
+            title,
+            technologies,
+            budget,
+            contact_email,
+            description
+        })
+            .then(doc => {
+                res.redirect('/gigs');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    } else {
+        res.render('add', {
+            errors,
+            title,
+            technologies,
+            budget,
+            contact_email,
+            description
+        });
+    }
+
+});
+
+
+// Search for gigs
+router.get('/search', (req, res, next) => {
+    let { term } = req.query;
+    term = term.toLowerCase()
+    Gig.findAll({ where: { technologies: { [Op.like]: '%' + term + '%' } } })
+        .then(gigs => {
+            res.render('gigs', { gigs })
+        })
+        .catch(err => console.log(err.message))
 });
 
 module.exports = router;
